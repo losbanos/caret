e7f6b2bee8f69c4b55b69c3a77f260eaccf50d02
[웹퍼블리셔팀]김태훈 <wwwholakr@gmail.com>
2017-02-27 14:02:17 +0900

    first drafted

---
+++ src/entry.js
@@ -0,0 +1,13 @@
+import './css/basic.css';
+import './css/layout.css';
+
+import 'bxslider';
+import './modules/sly';
+
+if(process.env.NODE_ENV !=='production') {
+    // require('index.html');
+}
+
+$(document).ready(function () {
+});
+
