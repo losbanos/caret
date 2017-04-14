a906183c68d57b95eac32b7eb90c0d6fb8888d4b
[웹퍼블리셔팀]김태훈 <wwwholakr@gmail.com>
2017-04-14 15:09:31 +0900

    debounce 메서드 추가

--- src/modules/addons/Utils.js
+++ src/modules/addons/Utils.js
@@ -24,6 +24,41 @@
 	window.CustomEvent = CustomEvent;
 
 	let $win = $(win), $doc = $(doc);
+	$.debounce = function (func, wait, immediate) {
+		let timeout, args, context, timestamp, result;
+
+		let later = function() {
+			let last = $.now() - timestamp;
+
+			if (last < wait && last >= 0) {
+				timeout = setTimeout(later, wait - last);
+			}
+			else {
+				timeout = null;
+				if (!immediate) {
+					result = func.apply(context, args);
+					if (!timeout) context = args = null;
+				}
+			}
+		};
+
+		return function () {
+			context = this;
+			args = arguments;
+			timestamp = $.now();
+
+			let callNow = immediate && !timeout;
+			if (!timeout) timeout = setTimeout(later, wait);
+
+			if (callNow) {
+				result = func.apply(context, args);
+				context = args = null;
+			}
+
+			return result;
+		};
+	};
+
 	$.preventActions = function (ev) {
 		ev = ev || window.event;
 		if (ev.stopPropagation && ev.preventDefault) {
